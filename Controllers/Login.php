2860dd295ca79cd6ac8e000b514c0a29bf21e8ac
hxstefghi <cccatuday1401qc@student.fatima.edu.ph>
2026-03-05 14:08:52 +0800

    feat: Implement login functionality with input validation and session management

---
+++ Controllers/Login.php
@@ -0,0 +1,65 @@
+<?php
+
+use Controllers\Database;
+
+class Login extends Database
+{
+
+  private $email, $password, $errors;
+
+  public function __construct()
+  {
+    session_start();
+
+    parent::__construct();
+
+    $this->email = $_POST['email'];
+    $this->password = $_POST['password'];
+
+    $this->errors = [];
+
+    $this->validateFields();
+  }
+
+  private function validateFields()
+  {
+    if (!isset($this->email) || empty($this->email)) {
+      $this->errors['email'] = 'The email field is required';
+    }
+
+    if (!isset($this->password) || empty($this->password)) {
+      $this->errors['password'] = 'The password field is required';
+    }
+
+    if (!empty($this->errors)) {
+      $_SESSION['errors'] = $this->errors;
+
+      header('Location: /login.php');
+      die();
+    }
+
+    $this->authenticate();
+  }
+
+  private function authenticate()
+  {
+    $query = "SELECT * FROM users WHERE email='$this->email' AND password='$this->password' LIMIT 1";
+
+    $result = $this->sql->query($query);
+
+    if ($result->num_rows > 0) {
+      $user = $result->fetch_assoc();
+
+      $_SESSION['user'] = $user;
+      header('Location: /home.php');
+      die();
+    } else {
+      $this->errors['email'] = '';
+      $this->errors['password'] = 'Invalid credentials';
+      $_SESSION['errors'] = $this->errors;
+
+      header("Location: /login.php");
+      die();
+    }
+  }
+}
--- login.php
+++ login.php
@@ -18,27 +18,17 @@ require_once('./Components/head.php');
 <body>
   <main class="flex flex-col min-h-screen justify-center max-w-md mx-auto">
     <div class="space-y-1 text-center mb-5">
-      <h1 class="text-3xl font-bold">Register an account</h1>
+      <h1 class="text-3xl font-bold">Login your account</h1>
       <p class="text-sm text-gray-500">Try this app. No credit card needed</p>
     </div>
 
     <div class="flex space-x-4 mb-5">
-      <button class="py-3 px-8 cursor-pointer rounded-lg border-1 border-gray-300 text-sm bg-gray-100"><i class="fa-brands fa-google"></i> Sign up with Google</button>
-      <button class="py-3 px-8 cursor-pointer rounded-lg border-1 border-gray-300 text-sm bg-gray-100"><i class="fa-brands fa-apple"></i> Sign up with Apple</button>
+      <button class="py-3 px-8 cursor-pointer rounded-lg border-1 border-gray-300 text-sm bg-gray-100"><i class="fa-brands fa-google"></i> Sign in with Google</button>
+      <button class="py-3 px-8 cursor-pointer rounded-lg border-1 border-gray-300 text-sm bg-gray-100"><i class="fa-brands fa-apple"></i> Sign in with Apple</button>
     </div>
 
-    <form action="./Routes/register.php" method="POST">
+    <form action="./Routes/login.php" method="POST">
       <div class="flex flex-col space-y-3">
-        <?php
-
-        $attribute = [
-          'name' => 'name',
-          'label' => 'Name',
-          'type' => 'text',
-        ];
-
-        require("./Components/input-field.php");
-        ?>
 
         <?php
 
@@ -62,14 +52,14 @@ require_once('./Components/head.php');
         require("./Components/input-field.php");
         ?>
 
-        <button type="submit" class="w-full cursor-pointer py-2 text-sm font-bold rounded-lg bg-lime-300 hover:bg-lime-400">Register</button>
+        <button type="submit" class="w-full cursor-pointer py-2 text-sm font-bold rounded-lg bg-lime-300 hover:bg-lime-400">Login</button>
       </div>
     </form>
 
     <div class="text-center mt-4 space-y-4">
       <p class="text-sm text-gray-400">or</p>
 
-      <p class="text-gray-400 text-sm">Already have an account? <a href="./login.php" class="font-bold text-black">Login</a></p>
+      <p class="text-gray-400 text-sm">Don't have an account? <a href="./register.php" class="font-bold text-black">Register</a></p>
     </div>
   </main>
 </body>
